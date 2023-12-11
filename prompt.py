6ebc442476abc45705051720772c8bef9afdad94
ramesh-15 <mudavathramesh458@gmail.com>
2023-12-11 16:43:23 +0530

    prompts data

--- prompt.py
+++ prompt.py
@@ -1,3 +1,5 @@
+
+
 SYSTEM_MESSAGE = """You are an AI assistant that is able to convert natural language into a properly formatted SQL query.
 
 The table you will be querying is called {table_1}. Here is the schema of the table:
