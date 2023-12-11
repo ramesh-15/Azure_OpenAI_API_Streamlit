318b7855c843a743012f1a5a78330cd7d249f237
ramesh-15 <mudavathramesh458@gmail.com>
2023-12-11 16:49:14 +0530

    azure openai connection

--- azure_openai.py
+++ azure_openai.py
@@ -2,30 +2,17 @@ from dotenv import load_dotenv
 import openai
 import os
 
-# Set your Azure API key
-# client = AzureOpenAI(
-#     # https://learn.microsoft.com/en-us/azure/ai-services/openai/reference#rest-api-versioning
-#     api_version="2023-07-01-preview",
-#     # https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/create-resource?pivots=web-portal#create-a-resource
-#     azure_endpoint="https://ahex-ai.openai.azure.com",
-#     azure_ad_token="e61550e0-4ac5-4a37-baee-83d18c04dfea",
-#     # azure_ad_token_provider=DefaultAzureCredential(),
-#     azure_ad_token_provider='9f56e29c-24cc-4304-b8a2-9a3f61de201e',
-# )
-
-#openai
-
 
 load_dotenv()
 
 #  azure connection
 openai.api_type = "azure"
 openai.api_version = "2023-05-15" 
-openai.api_key = "996d1769a4b14d96bd2ba7ec07f650f9"
-openai.api_base = "https://ahex-ai.openai.azure.com"
-deployment_name='ahex-sqldb'
+openai.api_key = "replace with your api_kay"
+openai.api_base = "replace with your resource endpoint"
+deployment_name='your deployment_name'
 
-def get_completion(system_message, user_message, deployment_name='ahex-sqldb', temperature=0, max_tokens=500) -> str:
+def get_completion(system_message, user_message, deployment_name='your deployment_name', temperature=0, max_tokens=500) -> str:
     """
     This method calls openai chatcompletion with the provided system message
     and user message(passed by user) and returns the content response returned 
