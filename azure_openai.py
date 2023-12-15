917b08f2899561126bddd4b1cc895c77562f7578
ramesh-15 <mudavathramesh458@gmail.com>
2023-12-15 10:50:13 +0530

    azure connection

--- azure_openai.py
+++ azure_openai.py
@@ -8,11 +8,11 @@ load_dotenv()
 #  azure connection
 openai.api_type = "azure"
 openai.api_version = "2023-05-15" 
-openai.api_key = "996d1769a4b14d96bd2ba7ec07f650f9"
-openai.api_base = "https://ahex-ai.openai.azure.com"
-deployment_name='ahex-sqldb'
+openai.api_key = "place your azure api key"
+openai.api_base = "place your azure endpoint"
+deployment_name='deployment_name'
 
-def get_completion(system_message, user_message, deployment_name='ahex-sqldb', temperature=0, max_tokens=500) -> str:
+def get_completion(system_message, user_message, deployment_name='deployment_name', temperature=0, max_tokens=500) -> str:
     """
     This method calls openai chatcompletion with the provided system message
     and user message(passed by user) and returns the content response returned 
