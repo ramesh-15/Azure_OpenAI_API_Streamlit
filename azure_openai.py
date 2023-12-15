ef48ab69964fec4de32f465a0ad19b3092a0f5cd
ramesh-15 <mudavathramesh458@gmail.com>
2023-12-15 10:52:39 +0530

    azure openai api connection

--- azure_openai.py
+++ azure_openai.py
@@ -8,20 +8,15 @@ load_dotenv()
 #  azure connection
 openai.api_type = "azure"
 openai.api_version = "2023-05-15" 
-openai.api_key = "996d1769a4b14d96bd2ba7ec07f650f9"
-openai.api_base = "https://ahex-ai.openai.azure.com"
-deployment_name='ahex-sqldb'
-
-# Set OpenAI API credentials and configuration new azure openai connectin
-# openai.api_type = "azure"
-# openai.api_version = "2023-05-15"
-# openai.api_key = "03f671cca3574127a43b8db73c7f7b57"
-# openai.api_base = "https://azure-openai-sql-services.openai.azure.com/"
-# deployment_name = 'deploy_azure_sql_db'
+openai.api_key = "open_api_key"
+openai.api_base = "endpoint"
+deployment_name='deployment_name'
 
 
 
-def get_completion(system_message, user_message, deployment_name='ahex-sqldb', temperature=0, max_tokens=1000) -> str:
+
+
+def get_completion(system_message, user_message, deployment_name='deployment_name', temperature=0, max_tokens=1000) -> str:
     """
     This method calls openai chatcompletion with the provided system message
     and user message(passed by user) and returns the content response returned 
--- main_app.py
+++ main_app.py
@@ -41,47 +41,84 @@ st.set_page_config(page_title="OpenAI With SQL", page_icon=":bar_chart:", layout
 st.title("OpenAI search on SQL Database")
 st.write("Ask anything about data in SQL database")
 # Input field for the user to type a message
+# options = ["Upload", "Excel", "CSV", "Data Source"]
 
-user_message = st.text_input("Enter your question:")
+# st.set_page_config(menu_items=dict(
+#     label="Select Data Source",
+#     icon="download",
+#     click=st.selectbox,
+#     args=(options,),
+   
+# ))
 
-if user_message:
-    
-    # Format the system message with the schema
-    formatted_system_message = SYSTEM_MESSAGE.format(schema_1=schemas, table_1=tables)
-    print('format:',formatted_system_message)
-    response = get_completion(formatted_system_message, user_message)
-    print('response:',response)
-    try:
-        print('in json')
-        dict_response = get_query_dict_from_response(response)
-        # print('anser from the def of dict response:',dict_response)
-        print('dict:',dict_response)
-        json_response = json.loads(dict_response)
+
+def upload_file():
+    uploaded_file = st.file_uploader("Choose a file", type=["csv", "xlsx"])
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file) if uploaded_file.name.endswith('.csv') else pd.read_excel(uploaded_file, engine='openpyxl')
+        st.write('file uploded successfully')
+        st.dataframe(df)
+        return df
+# Create the dropdown using st.selectbox
+selected_option = st.sidebar.selectbox("Select functionality", ["Upload","File Upload",  "Data Source"])
+
+# Perform action based on selected option
+if selected_option == "File Upload":
+    st.header("Upload File")
+    df_res =upload_file()
+    if df_res is not None:
+        input_query=st.text_input("Enter your question:")
+        result_df = df_res.query(input_query)
+        print("process data:",result_df)
+        st.write(result_df)
+elif selected_option == 'Data Source':
+    st.header('upload your data source')
+else:
+    user_message = st.text_input("Enter your question:")
+
         
-        query = json_response['query']
-        error = json_response['error']
-        # elif "I'm sorry" in response:
-        #     print('no query getting, provide more information !!!')
-        # else:
-        #     sql_query_pattern = re.compile(r'\bSELECT\b.*?;', re.DOTALL | re.IGNORECASE)
-        #     match = sql_query_pattern.search(response)
-        #     # print('query group:',match.group())
-        #     # print('query normal:',match)
-        #     query = match.group()
-        # # Display the generated SQL query
-        if query is None:
-            st.write(error)
-        else:
-            st.write("SQL Query:")
-            st.code(query, language="sql")
+    if user_message:
+        
+        # Format the system message with the schema
+        formatted_system_message = SYSTEM_MESSAGE.format(schema_1=schemas, table_1=tables)
+        print('format:',formatted_system_message)
+        response = get_completion(formatted_system_message, user_message)
+        print('response:',response)
+        try:
+            print('in json')
+            dict_response = get_query_dict_from_response(response)
+            # print('anser from the def of dict response:',dict_response)
+            print('dict:',dict_response)
+            json_response = json.loads(dict_response)
+            
+            query = json_response['query']
+            error = json_response['error']
+            # elif "I'm sorry" in response:
+            #     print('no query getting, provide more information !!!')
+            # else:
+            #     sql_query_pattern = re.compile(r'\bSELECT\b.*?;', re.DOTALL | re.IGNORECASE)
+            #     match = sql_query_pattern.search(response)
+            #     # print('query group:',match.group())
+            #     # print('query normal:',match)
+            #     query = match.group()
+            # # Display the generated SQL query
+            if query is None:
+                st.write(error)
+            else:
+                st.write("SQL Query:")
+                st.code(query, language="sql")
 
-            # Run the SQL query and display the results
-            conn = test_sql.get_db_connection()
-            sql_results = query_database(query, conn)
-            st.write("Query Results:")
-            st.dataframe(sql_results)
+                # Run the SQL query and display the results
+                conn = test_sql.get_db_connection()
+                sql_results = query_database(query, conn)
+                st.write("Query Results:")
+                st.dataframe(sql_results)
+        
+
+        except Exception as e:
+        #     # st.write(f"An error occurred: {e}")
+            st.write(response)
+
+
     
-
-    except Exception as e:
-    #     # st.write(f"An error occurred: {e}")
-        st.write(response)
+        
